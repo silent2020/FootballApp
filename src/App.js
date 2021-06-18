81d8a608e03ea861fb28b382ac750bf56f0d5e07
silent2020 <59793518+silent2020@users.noreply.github.com>
2021-06-18 23:34:49 +0430

    The token problem was fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import {Competitions,WeekNumber, WeeksNumber} from './components';
 import Styles from './App.Module.css';
+import {fetchData} from './api';
+import React from 'react';
+class App extends React.Component {
+  
+  async componentDidMount(){
+    const data = await fetchData();
+    console.log(data);
+  }
 
-function App() {
-  return (
-    <div className={Styles.container}>
-      <h1>Salam</h1> 
-      <WeeksNumber/>
-      <Competitions/>
-    </div>
-  );
+  render(){
+    return (
+      <div className={Styles.container}>
+        <h1>Salam</h1> 
+        <WeeksNumber/>
+        <Competitions/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,20 @@
 import axios from 'axios';
+
+var token='0fd0a99557434ac6b501daae21aa5241';
 const url = 'https://api.football-data.org/v2/competitions/2021/matches';
-/*const fetchData = async*/
+
+export const fetchData = async () =>{
+    try{
+        
+        const response = await axios.get(url,{
+            headers:{
+                'X-Auth-Token':token 
+            }
+        });
+        return response;
+        console.log(response);
+    }
+    catch(error){
+        
+    }
+}
